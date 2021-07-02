88b2232c701c9d123667603ff007c61d730bc0ba
meloniem <86366135+meloniem@users.noreply.github.com>
2021-07-02 10:19:32 -0500

    Update CheckersLogicTest.java

--- core/CheckersLogicTest.java
+++ core/CheckersLogicTest.java
@@ -1,7 +1,7 @@
 package core;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.*;
+
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
